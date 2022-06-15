60b07511065f82f007130c5cc9f77f0d46c4ea6e
Saffat Hasan <saffathasan@gmail.com>
2022-06-14 21:07:30 -0400

    fix: intialize by starting event loops instead of defaults
    buttons were not being disabled, so init via function call

--- src/main.py
+++ src/main.py
@@ -81,80 +81,83 @@ def launch_gui(league_api):
         title="AutoQr",
         layout=layout,
         icon=resource_path('favicon.ico'),
+        finalize=True,
     )
 
-    # Run lcu_api in the background, store its process in MAIN_PROC
-    background_proc = toggle_process(None, league_api)
+    # Run lcu_api in the background, store its process in background_proc
+    background_proc = update_state(league_api, window, background_proc=None, event='toggle', values=None)
 
     # GUI event loop handler
     while True:
         event, values = window.read()
+        background_proc = update_state(league_api, window, background_proc, event, values)
 
-        if event == 'toggle':
-            background_proc = toggle_process(background_proc, league_api)
-            window['status'].update(
-                'Running' if background_proc else 'Not running')
-            window['status'].update(
-                text_color='green' if background_proc else 'red')
-            window['toggle'].update('Stop' if background_proc else 'Start')
-            window['AUTO_LOBBY'].update(disabled=bool(background_proc))
-            window['QUEUE_ID'].update(
-                disabled=not cfg.AUTO_LOBBY or bool(background_proc))
-            window['PRIMARY_ROLE'].update(
-                disabled=not should_display_role_selection(cfg) or bool(background_proc))
-            window['SECONDARY_ROLE'].update(
-                disabled=not should_display_role_selection(cfg) or bool(background_proc))
-            window['AUTO_QUEUE'].update(disabled=bool(background_proc))
-            window['AUTO_ACCEPT'].update(disabled=bool(background_proc))
-            window['AUTO_SKIP_POSTGAME'].update(disabled=bool(background_proc))
 
-        # Code smell. Violates DRY.
-        # Use event, values = window.read() instead.
-        elif event == 'QUEUE_ID':
-            cfg.QUEUE_ID = QueueType[values['QUEUE_ID']]
-            window['PRIMARY_ROLE'].update(
-                disabled=not should_display_role_selection(cfg) or bool(background_proc))
-            window['SECONDARY_ROLE'].update(
-                disabled=not should_display_role_selection(cfg) or bool(background_proc))
+def update_state(league_api, window, background_proc, event, values=None):
+    cfg = league_api.config
+    if event == 'toggle':
+        background_proc = toggle_process(background_proc, league_api)
+        window['status'].update(
+            'Running' if background_proc else 'Not running')
+        window['status'].update(
+            text_color='green' if background_proc else 'red')
+        window['toggle'].update('Stop' if background_proc else 'Start')
+        window['AUTO_LOBBY'].update(disabled=bool(background_proc))
+        window['QUEUE_ID'].update(
+            disabled=not cfg.AUTO_LOBBY or bool(background_proc))
+        window['PRIMARY_ROLE'].update(
+            disabled=not should_display_role_selection(cfg) or bool(background_proc))
+        window['SECONDARY_ROLE'].update(
+            disabled=not should_display_role_selection(cfg) or bool(background_proc))
+        window['AUTO_QUEUE'].update(disabled=bool(background_proc))
+        window['AUTO_ACCEPT'].update(disabled=bool(background_proc))
+        window['AUTO_SKIP_POSTGAME'].update(disabled=bool(background_proc))
 
-        # Checkboxes toggle the value
-        elif event == 'AUTO_LOBBY':
-            cfg.AUTO_LOBBY = not cfg.AUTO_LOBBY
+    elif event == 'QUEUE_ID':
+        cfg.QUEUE_ID = QueueType[values['QUEUE_ID']]
+        window['PRIMARY_ROLE'].update(
+            disabled=not should_display_role_selection(cfg) or bool(background_proc))
+        window['SECONDARY_ROLE'].update(
+            disabled=not should_display_role_selection(cfg) or bool(background_proc))
 
-            # Cannot select a queue ID if we are not creating a lobby anyways
-            window['QUEUE_ID'].update(
-                disabled=not cfg.AUTO_LOBBY or bool(background_proc))
-            window['PRIMARY_ROLE'].update(
-                disabled=not should_display_role_selection(cfg) or bool(background_proc))
-            window['SECONDARY_ROLE'].update(
-                disabled=not should_display_role_selection(cfg) or bool(background_proc))
+    # Checkboxes toggle the value
+    elif event == 'AUTO_LOBBY':
+        cfg.AUTO_LOBBY = not cfg.AUTO_LOBBY
 
-        # TODO do not allow user to select the same role twice
-        elif event == 'PRIMARY_ROLE':
-            cfg.PRIMARY_ROLE = Roles[values['PRIMARY_ROLE']]
+        # Cannot select a queue ID if we are not creating a lobby anyways
+        window['QUEUE_ID'].update(
+            disabled=not cfg.AUTO_LOBBY or bool(background_proc))
+        window['PRIMARY_ROLE'].update(
+            disabled=not should_display_role_selection(cfg) or bool(background_proc))
+        window['SECONDARY_ROLE'].update(
+            disabled=not should_display_role_selection(cfg) or bool(background_proc))
 
-        elif event == 'SECONDARY_ROLE':
-            cfg.SECONDARY_ROLE = Roles[values['SECONDARY_ROLE']]
+    # TODO do not allow user to select the same role twice
+    elif event == 'PRIMARY_ROLE':
+        cfg.PRIMARY_ROLE = Roles[values['PRIMARY_ROLE']]
 
-        elif event == 'AUTO_QUEUE':
-            cfg.AUTO_QUEUE = not cfg.AUTO_QUEUE
+    elif event == 'SECONDARY_ROLE':
+        cfg.SECONDARY_ROLE = Roles[values['SECONDARY_ROLE']]
 
-        elif event == 'AUTO_ACCEPT':
-            cfg.AUTO_ACCEPT = not cfg.AUTO_ACCEPT
+    elif event == 'AUTO_QUEUE':
+        cfg.AUTO_QUEUE = not cfg.AUTO_QUEUE
 
-        elif event == 'AUTO_SKIP_POSTGAME':
-            cfg.AUTO_SKIP_POSTGAME = not cfg.AUTO_SKIP_POSTGAME
+    elif event == 'AUTO_ACCEPT':
+        cfg.AUTO_ACCEPT = not cfg.AUTO_ACCEPT
 
-        elif event == 'AUTO_PLAY_AGAIN':
-            cfg.AUTO_PLAY_AGAIN = not cfg.AUTO_PLAY_AGAIN
+    elif event == 'AUTO_SKIP_POSTGAME':
+        cfg.AUTO_SKIP_POSTGAME = not cfg.AUTO_SKIP_POSTGAME
 
-        elif event == sg.WINDOW_CLOSED:
-            if background_proc:
-                background_proc.terminate()
-            break
+    elif event == 'AUTO_PLAY_AGAIN':
+        cfg.AUTO_PLAY_AGAIN = not cfg.AUTO_PLAY_AGAIN
 
-        league_api.update_config(cfg)
+    elif event == sg.WINDOW_CLOSED:
+        if background_proc:
+            background_proc.terminate()
 
+    league_api.update_config(cfg)
+
+    return background_proc
 
 def should_display_role_selection(config: Config):
     if not config.AUTO_LOBBY:
